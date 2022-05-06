6444a7fb2e6f2698e74cdd8c6aefa833de108166
Alexander Adhyatma <alex@asiatech.dev>
2022-05-06 09:51:59 +0700

    make generic request call

--- client.go
+++ client.go
@@ -1,6 +1,37 @@
 package gosb
 
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+)
+
 // Config provides SendBird config
-// contains API Token, Application ID, and
+// contains API Token, Application ID, and API version of Sendbird
+// by default it will use version 3 of the Sendbird API
 type Config struct {
+	APIToken           string
+	ApplicationID      string
+	SendbirdApiVersion int
+}
+
+// BaseURL returns base URL for any endpoints
+func (c Config) baseURL() string {
+	return fmt.Sprintf("https://api-%s.sendbird.com/v%d", c.ApplicationID, c.SendbirdApiVersion)
+}
+
+// Client contains default http client to use
+// as well as the base url to use
+type Client struct {
+	appID   string
+	hc      *resty.Client
+	baseURL string
+}
+
+// NewClient will create a new *Client instance
+func NewClient(cfg Config) *Client {
+	return &Client{
+		appID:   cfg.ApplicationID,
+		hc:      defaultHC(cfg.APIToken, nil),
+		baseURL: cfg.baseURL(),
+	}
 }
--- http_client.go
+++ http_client.go
@@ -1,12 +1,16 @@
 package gosb
 
 import (
+	"context"
+
 	"github.com/go-resty/resty/v2"
+
+	"github.com/alexadhy/gosb/dto"
 )
 
 // defaultHC returns default http.Client (*resty.Client) to use
 // in facing SendBird API
-func defaultHC(opts ...resty.Option) *resty.Client {
+func defaultHC(apiToken string, opts ...resty.Option) *resty.Client {
 	options := &resty.Options{}
 	c := resty.New()
 
@@ -14,5 +18,62 @@ func defaultHC(opts ...resty.Option) *resty.Client {
 		opt(options)
 	}
 	c.SetRetryCount(3)
+
+	c.Header.Add("Api-Token", apiToken)
 	return c
+}
+
+type urler interface {
+	URL(string) string
+}
+
+func makeRequestURL(input urler, c *Client) (string, error) {
+	u := input.URL(c.baseURL)
+	if u == "" {
+		return "", dto.NewErr(dto.ErrEmptyURLValue, "empty request URL")
+	}
+	return u, nil
+}
+
+func makeRequest[T any](ctx context.Context, hc *resty.Client, fn func(request *resty.Request) (*resty.Response, error)) (*T, error) {
+	var out T
+	resp, err := fn(hc.R().
+		SetResult(&out).
+		SetError(&out).
+		SetContext(ctx))
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := resp.Result().(*T)
+	return result, nil
+}
+
+func getRequest[T any](ctx context.Context, hc *resty.Client, u string) (*T, error) {
+	return makeRequest[T](ctx, hc, func(request *resty.Request) (*resty.Response, error) {
+		return request.Get(u)
+	})
+}
+
+func postRequest[T any](ctx context.Context, hc *resty.Client, u string, body any) (*T, error) {
+	return makeRequest[T](ctx, hc, func(request *resty.Request) (*resty.Response, error) {
+		return request.
+			SetBody(body).
+			Post(u)
+	})
+}
+
+func putRequest[T any](ctx context.Context, hc *resty.Client, u string, body any) (*T, error) {
+	return makeRequest[T](ctx, hc, func(request *resty.Request) (*resty.Response, error) {
+		return request.
+			SetBody(body).
+			Put(u)
+	})
+}
+
+func deleteRequest[T any](ctx context.Context, hc *resty.Client, u string) (*T, error) {
+	return makeRequest[T](ctx, hc, func(request *resty.Request) (*resty.Response, error) {
+		return request.Delete(u)
+	})
 }
